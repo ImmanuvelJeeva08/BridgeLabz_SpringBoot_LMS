16497921e9c272fc5aa9d04baba09c693a997e78
ImmanuvelJeeva08 <immanuveljeeva2000@gmail.com>
2022-03-16 20:42:56 +0530

    Sucessfully added

--- LMS/src/main/java/com/example/lms/LmsApplication.java
+++ LMS/src/main/java/com/example/lms/LmsApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example.lms")
 public class LmsApplication extends SpringBootServletInitializer {
 
     @Bean
